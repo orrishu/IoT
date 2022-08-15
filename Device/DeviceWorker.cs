fd48e690dc68655587b588f6295a2178783a24e7
Ori Shuchami <ori_s@ifat.com>
2022-08-15 11:15:20 +0300

    Added humidity to device and added loop to increase\decrease by faucet state (debug mode).

--- Device/DeviceWorker.cs
+++ Device/DeviceWorker.cs
@@ -18,6 +18,7 @@ namespace Device
         private readonly Exchange _exchange;
         private readonly string _device_id = "Device_1";    // Todo: move to config.
         private bool _faucetOpen = false;
+        private int _humidity = 0;
 
         public DeviceWorker(IBus bus, ILogger<DeviceWorker> logger)
         {
@@ -43,7 +44,7 @@ namespace Device
                     stoppingToken);
 
             // Each device publishes to same queue ... :
-            int count = 100;
+            /*int count = 100;
             while (!stoppingToken.IsCancellationRequested && count-- > 0 && !_faucetOpen)
             {
                 try
@@ -65,6 +66,21 @@ namespace Device
                     _logger.LogError("Error in status message publish: {error}", e.Message);
                 }
 
+            }*/
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_faucetOpen)
+                {
+                    _logger.LogInformation("Faucet open, dehydrating...");
+                    await DehydrateHumidity(stoppingToken);
+                }
+                else
+                {
+                    _logger.LogInformation("Faucet closed, hydrating...");
+                    await HydrateHumidity(stoppingToken);
+                }
+                await Task.Delay(1000, stoppingToken);
             }
 
             //wait until service is stopped
@@ -73,6 +89,68 @@ namespace Device
             stoppingToken.Register(tcs.SetResult);
 
             await tcs.Task;
+        }
+
+        private async Task HydrateHumidity(CancellationToken stoppingToken)
+        {
+            int count = 0;
+            while (!stoppingToken.IsCancellationRequested && count++ < 100 && !_faucetOpen)
+            {
+                try
+                {
+                    _logger.LogInformation($"HydrateHumidity: current humidity is {_humidity}, faucet is closed");
+                    _humidity++;
+                    //var body = new DeviceStatusMessage
+                    //{
+                    //    DeviceId = _device_id,
+                    //    Humidity = _humidity,
+                    //    IsFaucetOpen = _faucetOpen
+                    //};
+                    ////use the simple config:
+                    //await _bus.SendReceive.SendAsync(_queue.Name, body, cancellationToken: stoppingToken);
+
+                    await Task.Delay(100, stoppingToken);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError("Error in status message publish: {error}", e.Message);
+                }
+            }
+            if (count >= 100)
+            {
+                _faucetOpen = true;
+            }
+        }
+
+        private async Task DehydrateHumidity(CancellationToken stoppingToken)
+        {
+            int count = 100;
+            while (!stoppingToken.IsCancellationRequested && count-- > 0 && _faucetOpen)
+            {
+                try
+                {
+                    _logger.LogInformation($"DehydrateHumidity: current humidity is {_humidity}, faucet is open");
+                    _humidity--;
+                    //var body = new DeviceStatusMessage
+                    //{
+                    //    DeviceId = _device_id,
+                    //    Humidity = _humidity,
+                    //    IsFaucetOpen = _faucetOpen
+                    //};
+                    ////use the simple config:
+                    //await _bus.SendReceive.SendAsync(_queue.Name, body, cancellationToken: stoppingToken);
+
+                    await Task.Delay(100, stoppingToken);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError("Error in status message publish: {error}", e.Message);
+                }
+            }
+            if (count <= 0)
+            {
+                _faucetOpen = false;
+            }
         }
 
         private async Task HandleCommand(GatewayCommandMessage commandMessage, CancellationToken cancellationToken)
